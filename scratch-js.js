bb55d8726d058578b3d8738826e18b926f097484
Baran Skistad <bskist01@isd77.k12.mn.us>
2016-06-10 22:24:15 +0000

    Math functions

--- scratch-js.js
+++ scratch-js.js
@@ -2,6 +2,7 @@ var exports = module.exports = {};
 const vm = require("vm");
 const process = require("process");
 var time = require("time");
+var int = 1234;
 
 var monthNames = ['January', 'Febuary', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
@@ -14,7 +15,7 @@ exports.hour           = time.localtime(Date.now()/1000).hours;
 exports.minute         = time.localtime(Date.now()/1000).minutes;
 exports.second         = time.localtime(Date.now()/1000).seconds;
 exports.timezone       = time.localtime(Date.now()/1000).timezone;
-
+exports.pi             = Math.PI;
 
 exports.repeat = function(times, callback) {
     for (var i = 0; i < times; i++) {
@@ -22,7 +23,34 @@ exports.repeat = function(times, callback) {
     }
 };
 
-exports.wait = function(secs) {
-    setTimeout(process.stdin.resume, secs * 1000);
-    process.stdin.pause();
+exports.join = function(jointhis, andthis) {
+    return jointhis + andthis;
+};
+
+exports.sqrt = function(number) {
+    if (typeof number == typeof int) {
+        return Math.sqrt(number);
+    }
+};
+
+exports.sin = function(number) {
+    if (typeof number == typeof int) {
+        return Math.sin(number);
+    }
+};
+
+exports.cos = function(number) {
+    if (typeof number == typeof int) {
+        return Math.cos(number);
+    }
+};
+
+exports.tan = function(number) {
+    if (typeof number == typeof int) {
+        return Math.tan(number);
+    }
+};
+
+exports.random = function(lowest, highest) {
+    return Math.ceil(Math.random() * (highest - lowest + 1) + lowest - 1);
 };
--- test.js
+++ test.js
@@ -3,3 +3,5 @@ var scratch = require("./scratch-js.js");
 scratch.repeat(3, function() {
     console.log('Repeat this.');
 });
+
+console.log(scratch.random(5, 900));
